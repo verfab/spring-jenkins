684560621d2c7ddec96caf31dcfcf460811dfafc
fabian <verena.fabian@unitrade.com>
2024-08-27 16:33:34 +0200

    remove logger statement

--- src/test/java/com/project/spring_jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/project/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -16,7 +16,6 @@ class SpringJenkinsApplicationTests {
 	@Test
 	 public void contextLoads() {
 		logger.info("Test case executing...");
-		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);
 	}
 
