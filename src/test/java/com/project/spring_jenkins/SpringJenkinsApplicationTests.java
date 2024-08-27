766ec1ba67700f70baac98c8ccbf87834fb16817
fabian <verena.fabian@unitrade.com>
2024-08-27 10:48:35 +0200

    Fix commit

--- src/test/java/com/project/spring_jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/project/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -14,7 +14,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	 public String contextLoads() {
+	 public void contextLoads() {
 		logger.info("Test case executing...");
 		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);
